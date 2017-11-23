d7f868e6d7ae8bccce5a405e4fe67722ffce4502
edylle <edyllelandy@gmail.com>
2017-11-23 20:05:50 -0300

    - Updates

--- src/main/java/com/hockey/startup/ApplicationStartup.java
+++ src/main/java/com/hockey/startup/ApplicationStartup.java
@@ -1,5 +1,7 @@
 package com.hockey.startup;
 
+import java.util.Random;
+
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
@@ -20,11 +22,18 @@ public class ApplicationStartup implements ApplicationListener<ApplicationReadyE
 
 		System.out.println("CREATING MOCK SEATS");
 
+		Random n = new Random();
 		int i = 1;
+
 		for (i = 1; i <= 100; i++) {
 
 			String credentials = StringUtils.leftPad(String.valueOf(i), 3, "0");
-			seatRepository.save(new Seat(credentials, credentials));
+			Seat s = new Seat(credentials, credentials);
+
+			// generate random points ranging from 1 to 100
+			s.setPoints(n.nextInt(100 - 1 + 1) + 1);
+
+			seatRepository.save(s);
 		}
 
 		System.out.println(--i + " MOCK SEATS CREATED");
